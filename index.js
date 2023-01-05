5572f2c2e64b360a518a03e235183a138ec98841
Vedant pople <vedantpople4@gmail.com>
2023-01-05 22:15:54 +0530

    REST-api done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const Joi = require('joi');
+const app = express();
+app.use(express.json());
 
 const pages = [
     {id: 1, name: 'Page 1'},
@@ -23,6 +25,57 @@ app.get('/api/pages/:id', (req,res)=>{
     if(!page) res.status(404).sendStatus('Page does not exist')
     res.send(page);
 });
+
+app.post('/api/pages', (req, res)=>{
+    
+    const { error } = validatePage(req.body);
+    if(error){
+        res.status(404).send(result.error.details[0].message);
+        return;
+    }
+
+    const page = {
+        id: page.length+1,
+        name: req.body.name 
+    };
+    pages.push(page);
+    res.send(page);
+});
+
+app.put('/api/pages/:id', (req,res)=>{
+    const page = pages.find(c=> c.id === parseInt(req.params.id));
+    if(!page) {
+        res.status(404).sendStatus('Page does not exist')
+        return 
+    }
+    const { error } = validatePage(req.body);
+    if(error){
+        res.status(404).send(error.details[0].message);
+        return;
+    }
+
+    page.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req,res)=>{
+    const page = pages.find(c=> c.id === parseInt(req.params.id));
+    if(!page)  return res.status(404).sendStatus('Page does not exist')
+
+    //Delete
+    const index = pages.indexOf(page);
+    pages.splice(index, 1);
+
+    res.send(page);
+});
+
+function validatePage(page){
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.valid(schema, page);
+}
 
 //port assignment
 const port = process.env.PORT || 3000
